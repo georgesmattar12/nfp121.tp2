7ee5a3734ea06e59266238b74c4bcb1e10f3b15d
2419n <94062223+georgesmattar12@users.noreply.github.com>
2023-12-16 11:49:48 +0200

    COMMITED

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -2,37 +2,71 @@ package question2;
 
 
 /**
- * Décrivez votre classe FahrenheitCelsius ici.
- * 
- * @author (votre nom) 
- * @version (un numéro de version ou une date)
+ * Classe-test FahrenheitCelsiusTest.
+ *
+ * @author  Georges Mattar
+ * @version V1.0
+ *
+ * Les classes-test sont documentées ici :
+ * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
+ * et sont basées sur le document © 2002 Robert A. Ballance intitulé
+ * «JUnit: Unit Testing Framework».
+ *
+ * Les objets Test (et TestSuite) sont associés aux classes à tester
+ * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
+ * se nommera NameTest.java); les deux se retrouvent dans le même paquetage.
+ * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
+ * qui sont vraies pour chaque méthode Test à exécuter.  Il peut y avoir
+ * plus d'une méthode Test dans une classe Test; leur ensemble forme un
+ * objet TestSuite.
+ * BlueJ découvrira automatiquement (par introspection) les méthodes
+ * Test de votre classe Test et générera la TestSuite conséquente.
+ * Chaque appel d'une méthode Test sera précédé d'un appel de setUp(),
+ * qui réalise les engagements, et suivi d'un appel à tearDown(), qui les
+ * détruit.
  */
-public class FahrenheitCelsius{
+public class FahrenheitCelsiusTest extends junit.framework.TestCase
+{
+    // Définissez ici les variables d'instance nécessaires à vos engagements;
+    // Vous pouvez également les saisir automatiquement du présentoir
+    // à l'aide du menu contextuel "Présentoir --> Engagements".
+    // Notez cependant que ce dernier ne peut saisir les objets primitifs
+    // du présentoir (les objets sans constructeur, comme int, float, etc.).
 
-     /** le point d'entrée de cette application, 
-      * dont le commentaire est à compléter
-      *
-      *  @param args ...
-      */
-     public static void main(String[] args){
-       try{
-       
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
-       
-     }
-     
-     /** 
-      * la méthode à compléter. 
-      *   @param f la valeur en degré Fahrenheit
-      *   @return  la conversion en degré Celsius
-      */
-     public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
-       // ...
-     }
+    /**
+     * Constructeur de la classe-test FahrenheitCelsiusTest
+     */
+    public FahrenheitCelsiusTest()
+    {
+    }
+
+    /**
+     * Met en place les engagements.
+     *
+     * Méthode appelée avant chaque appel de méthode de test.
+     */
+    protected void setUp() // throws java.lang.Exception
+    {
+        // Initialisez ici vos engagements
+
+    }
+
+    /**
+     * Supprime les engagements
+     *
+     * Méthode appelée après chaque appel de méthode de test.
+     */
+    protected void tearDown() // throws java.lang.Exception
+    {
+        //Libérez ici les ressources engagées par setUp()
+    }
+
+    /**
+     * Il ne vous reste plus qu'à définir une ou plusieurs méthodes de test.
+     * Ces méthodes doivent vérifier les résultats attendus à l'aide d'assertions
+     * assertTrue(<boolean>).
+     * Par convention, leurs noms devraient débuter par "test".
+     * Vous pouvez ébaucher le corps grâce au menu contextuel "Enregistrer une méthode de test".
+     */
 
 }
